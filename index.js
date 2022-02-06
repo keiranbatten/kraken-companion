42066a84404e88031608eadd90766b0041481f3a
Keiran Batten <keiranbatten1998@gmail.com>
2022-02-06 16:43:51 +1300

    added views, login controller and route

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+app.set('view engine', 'ejs');
 const PORT = process.env.PORT || 4000;
 const KRAKEN_KEY = process.env.KRAKEN_KEY || "";
 const KRAKEN_SECRET = process.env.KRAKEN_SECRET || "";
 const KrakenClient = require('kraken-api');
 const kraken = new KrakenClient(KRAKEN_KEY, KRAKEN_SECRET);
+
+app.use('/', require('./routes/login'));
 
 app.listen(PORT, console.log(`Server listening on port ${PORT}`));
